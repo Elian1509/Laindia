a77b63862a223edd8255e042bcaad85aefbb8c21
Beelzebud <eliholsan1509@gmail.com>
2025-09-30 20:51:32 -0500

    Commit inicial - Configuracion inicial

---
+++ src/main/java/com/beelzebud/invSales_System/InvSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.beelzebud.invSales_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvSalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beelzebud/invSales_System/InvSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beelzebud.invSales_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvSalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
